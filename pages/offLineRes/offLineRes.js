6b44f9fa01213bc4faa0d25d3d9e90b5dc443e65
Tianhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <91210927+Tianhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2021-10-08 00:20:13 +0800

    Add a feature
    Consider about the draw

--- pages/offLineRes/offLineRes.js
+++ pages/offLineRes/offLineRes.js
@@ -8,13 +8,36 @@ Page({
     dies:[]
   },
   onLoad: function (options) {
+    console.log(app.globalData.res)
     for (let index = 0; index < app.globalData.res.length; index++) {
       if (app.globalData.res[this.data.maxIndex] < app.globalData.res[index]) {
         this.data.maxIndex = index
       }
     }
+    var resArr = app.globalData.res.reduce((obj, num) => {
+      if (num in obj) {
+        obj[num]++
+      } else {
+        obj[num] = 1
+      }
+      return obj
+    }, {})
+    var n=""
+    if (resArr[app.globalData.res[this.data.maxIndex]]>1) {
+      for (let index = 0; index < app.globalData.res.length; index++) {
+        if (app.globalData.res[this.data.maxIndex] == app.globalData.res[index]) {
+          n+=app.globalData.value[index]+" "
+        }
+      }
+      this.setData({
+        name: n,
+      })
+    }else{
+      this.setData({
+        name: app.globalData.value[this.data.maxIndex],
+      })
+    }
     this.setData({
-      name: app.globalData.value[this.data.maxIndex],
       value:app.globalData.value,
       dies:app.globalData.dies
     })
